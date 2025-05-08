662d7d8da7a2c3ebc4a52aee39c09cc933f4d9f3
mikaeljohanjohnsson@gmail.com <mikaeljohanjohnsson@gmail.com>
2025-05-08 18:11:39 +0200

    solve uppg3

--- uppg3.js
+++ uppg3.js
@@ -6,10 +6,12 @@ function uppg3() {
     // deklarera två variabler, a och b
     // tilldela värdena 5 och 10 till variablerna
     // skriv ut summan av a och b i konsolen
-
-
-
+    const a = 5;
+    const b = 10;
+    console.log(a + b);
+    
 }
 
 
 module.exports = { uppg3 };
+uppg3();
