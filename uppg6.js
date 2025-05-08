d55237e6d8e350e44145b4d89cac7878728b0dae
mikaeljohanjohnsson@gmail.com <mikaeljohanjohnsson@gmail.com>
2025-05-08 18:21:46 +0200

    solve uppg6

--- uppg6.js
+++ uppg6.js
@@ -4,8 +4,11 @@ function uppg6(){
 
     // Skapa en array med namn som du döper till names
     // Loopa igenom arrayen och skriv ut alla namn i konsolen
-
- 
+    const names = ["johan", "linda", "kalle", "anna", "erik"];
+    for(let x = 0; x < names.length; x++){
+        console.log(names[x])
+    }
 }
 
 module.exports = { uppg6 };
+uppg6();
