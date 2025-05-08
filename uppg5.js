a50c7f0ffdb3516cbfab7b69ed91af0da59b70ea
mikaeljohanjohnsson@gmail.com <mikaeljohanjohnsson@gmail.com>
2025-05-08 18:16:15 +0200

    solve uppg5

--- uppg5.js
+++ uppg5.js
@@ -3,9 +3,14 @@ function uppg5() {
     // skriv en if-sats som jämför två tal
     // om det första talet är större än det andra, skriv ut "Det första talet är större än det andra"
     // annars skriv ut "Det andra talet är större än det första"
-
+    if (1 > 2){
+        console.log("Det första talet är större än det andra");
+    } else {
+        console.log("Det andra talet är större än det första");
+    };
 
 
 }
 
 module.exports = { uppg5 };
+uppg5();
