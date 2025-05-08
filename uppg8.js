167bdef5f3ac2a9abedcbc41a3bd48f92e2e2600
mikaeljohanjohnsson@gmail.com <mikaeljohanjohnsson@gmail.com>
2025-05-08 18:42:04 +0200

    solve uppg8

--- uppg8.js
+++ uppg8.js
@@ -10,8 +10,25 @@ function uppg8(){
 //  över 30 år som skrivs ut i konsolen
 
 // anropa funktionen och skicka med arrayen som argument
+    const persons = [
+        {name: "johan", age: 20},
+        {name: "lina", age: 25},
+        {name: "jasmine", age: 29},
+        {name: "erik", age: 45},
+        {name: "göran", age: 55}
+    ]
 
+    function over30(persons){
+        for(let person of persons){
+            if(person.age > 30){
+                console.log(person.name)
+            }
+        }  
+    };
+    
+    over30(persons);
 
 }
 
 module.exports = { uppg8 };
+uppg8();
