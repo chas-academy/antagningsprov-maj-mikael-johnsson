327127ae2ea18eb4de9c75cb076d7689dc5358b0
mikaeljohanjohnsson@gmail.com <mikaeljohanjohnsson@gmail.com>
2025-05-10 12:21:10 +0200

    solve uppg9

--- uppg9.js
+++ uppg9.js
@@ -7,8 +7,20 @@ function uppg9 (){
     // resultatet ska loggas i formatet: talet och sen ordet "jämt" eller "udda"
 
     // anropa funktionen och skicka med en array som argument
+    function sort(numbers){
+        numbers.forEach(e => {
+            if(e % 2 == 0){
+                console.log(e + " jämt");
+            } else {
+                console.log(e + " udda");
+            };
+        });
+    };
 
-  
+    const numbers = [1, 6, 3, 12, 4];
+
+    sort(numbers);
 }
 
 module.exports = { uppg9 };
+uppg9();
